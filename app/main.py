d5742cdca7e1cd5a7ec434325f65ff6a3d518701
Mitchell Busby <mitchbusby@gmail.com>
2019-09-07 12:10:37 +1000

    Adds styling

--- app/main.py
+++ app/main.py
@@ -7,10 +7,12 @@ app = Flask(
     template_folder='templates',
 )
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route('/')
 def index():
     # Hosts the main part of the application
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
