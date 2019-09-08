1faef012cb780562551179ded86adcec698ac282
Mitchell Busby <mitchbusby@gmail.com>
2019-09-08 13:51:44 +1000

    Adds randomisation based on regular sydney suburbs now

--- app/src/fetchRegions.ts
+++ app/src/fetchRegions.ts
@@ -1,4 +1,4 @@
-import { RegionModelModel } from "./fetchTile";
+import { RegionModelModel, TileModel, region$ } from "./fetchTile";
 import * as RxJS from "rxjs";
 
 let regions$ = new RxJS.ReplaySubject<Regions>(1);
@@ -12,6 +12,20 @@ const fetchRegions = async () => {
   return json;
 };
 
+const fetchSpecificTiles = async (region) => {
+    // todo: use this function in similarityMap.ts
+    const result = await fetch("/api/tiles", {
+        method: "POST",
+        body: JSON.stringify(region),
+        headers: {
+          "Content-Type": "application/json"
+        }
+    });
+
+    const json = (await result.json()) as TileModel[];
+    return json;
+}
+
 
 /**
  * Responsible for bootstrapping the regions
@@ -20,4 +34,4 @@ const initRegions = () => {
     fetchRegions();
 }
 
-export { regions$, initRegions };
+export { regions$, initRegions, fetchSpecificTiles };
--- app/src/randomiseButton.ts
+++ app/src/randomiseButton.ts
@@ -1,9 +1,28 @@
-import {loadingRegion$} from './fetchTile';
+import {loadingRegion$, region$} from './fetchTile';
+import { regions$, fetchSpecificTiles } from './fetchRegions';
+
+const chooseRandomElement = arr => arr[Math.floor(Math.random() * arr.length)];
 
 const initialise = () => {
+    const randomRealSuburbBtn = document.querySelector("#real-suburb-btn");
+
+    regions$.subscribe((allRegions) => {
+        randomRealSuburbBtn.addEventListener("click", async () => {
+            // shuffle
+            const region = chooseRandomElement(Object.values(allRegions));
+
+            // get tiles
+            const tiles = await fetchSpecificTiles(region);
+
+            // set the region
+            region$.next({ model: region, tiles });
+        });
+    });
+
     loadingRegion$.subscribe((loading) => {
         document.querySelector("#randomise-btn")
             .toggleAttribute('disabled', loading);
+        randomRealSuburbBtn.toggleAttribute('disabled', loading);
     });
 }
 
